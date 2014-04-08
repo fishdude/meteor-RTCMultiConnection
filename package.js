1c8ec7ff5e09e10c530f20bb2e276632788a86ee
fishdude <d.n.fischer9@gmail.com>
2014-04-08 13:31:37 -0700

    fix

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "A library for cross-browser media streaming; screen sharing; data/file sharing; renegotiation; and much more. An all-in-one wrapper for RTCWeb API packaged for Meteor!"
+  summary: "A library for cross-browser media streaming, screen sharing, data/file sharing, renegotiation, and much more. An all-in-one wrapper for RTCWeb API packaged for Meteor!"
 });
 
 Package.on_use(function (api, where) {
